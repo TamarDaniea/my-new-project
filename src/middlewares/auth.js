52dc7235e32b8f021cfc4e20e66f7c4b5950aa7d
tamar daniel <tamard3025@gmail.com>
2025-08-25 13:32:48 +0300

    fixed

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,6 +4,7 @@ const User = require('../models/User');
 
 const auth = async (req, res, next) => {
     const authHeader = req.headers.authorization;
+   
 
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({ error: 'No token provided' });
@@ -41,45 +42,5 @@ const auth = async (req, res, next) => {
 module.exports = auth;
 
 
-
-
-// const admin = require('firebase-admin');
-// const User = require('../models/User');
-
-// module.exports = async function authenticate(req, res, next) {
-//     const authHeader = req.headers.authorization;
-
-//     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-//         return res.status(401).json({ error: req.t ? req.t('auth.no_token') : 'No token provided' });
-//     }
-
-//     const token = authHeader.split(' ')[1];
-
-//     try {
-//         const decodedToken = await admin.auth().verifyIdToken(token);
-//         const firebase_uid = decodedToken.uid;
-
-//         // ⛔️ בדיקת האם המשתמש אישר את המייל שלו
-//         if (!decodedToken.email_verified) {
-//             return res.status(403).json({ error: req.t ? req.t('auth.email_not_verified') : 'Email not verified. Please check your inbox.' });
-//         }
-
-//         // אחזור פרטי המשתמש מה-DB
-//         const userFromDb = await User.getById(firebase_uid);
-//         if (!userFromDb) {
-//             return res.status(401).json({ error: req.t ? req.t('auth.user_not_found') : 'User not found in database' });
-//         }
-
-//         req.user = {
-//             firebase_uid: firebase_uid,
-//             role: userFromDb.role
-//         };
-
-//         next();
-//     } catch (error) {
-//         console.error('Firebase authentication error:', error);
-//         return res.status(401).json({ error: req.t ? req.t('auth.invalid_token') : 'Invalid token or authentication failed' });
-//     }
-// };
 
 
---
+++ src/utils/jwt.js
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+
+export function generateToken(user) {
+    const result = jwt.sign(
+        { userId: user._id, role: user.role, username: user.username },
+        process.env.SECRET_KEY,
+        { expiresIn: "1h" }
+    );
+    return result;
+}
+ 
